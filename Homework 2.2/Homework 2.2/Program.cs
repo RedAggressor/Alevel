2c182b543f9c866b8d2ed205c9ac5006681a7704
MaksymBabych <cadacred@gmail.com>
2024-01-04 21:51:53 +0200

    add readabillyty

--- Homework 2.2/Homework 2.2/Program.cs
+++ Homework 2.2/Homework 2.2/Program.cs
@@ -8,8 +8,11 @@ namespace Homework_2._2
          {
 
             int[] A = new int[20];
+
             int[] B = new int[20];
+
             A.SetRandomElement();
+
             for (int i = 0; i < A.Length; i++)
             {
                 if (A[i] <= 888)
@@ -25,18 +28,25 @@ namespace Homework_2._2
                     if (B[j - 1] < B[j])
                     {
                         int res = B[j];
+
                         B[j] = B[j - 1];
+
                         B[j - 1] = res;
                     }
                 }
             }
+
             Console.WriteLine("Array B after sort is decending order:");
+
             foreach (var item in B)
             {
                 Console.Write(item + " ");
             }
+
             Console.WriteLine();
+
             Console.WriteLine("Press any key to exit...");
+
             Console.ReadKey();
          }
     }
