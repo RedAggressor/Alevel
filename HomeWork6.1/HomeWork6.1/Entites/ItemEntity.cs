d4891e701324ece5c0a91d95dc2e37c8e4d86ad2
MaksymBabych <cadacred@gmail.com>
2024-01-13 14:40:35 +0200

    add HomeWork6

---
+++ HomeWork6.1/HomeWork6.1/Entites/ItemEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Entites
+{
+    internal class ItemEntity
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Amount { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ HomeWork6.1/HomeWork6.1/Service/NotificationService.cs
@@ -0,0 +1,21 @@
+﻿using Models;
+
+namespace Service
+{
+    internal class NotificationService
+    {
+        private LoggerService _loggerService;
+
+        public NotificationService(LoggerService loggerService) 
+        { 
+            _loggerService = loggerService;
+        }
+
+        public void Notify(string message)
+        {
+            string massageToNotify = $"This message send you to email: {message}";
+
+            _loggerService.log(LogType.Info, massageToNotify);
+        }
+    }
+}
