ab2fe140d16f6c66a75c50c44394ed7c34d5bdf6
MaksymBabych <cadacred@gmail.com>
2024-02-11 14:21:28 +0200

    add HomeWork 13

---
+++ HomeWork13/HomeWork13/Program.cs
@@ -0,0 +1,46 @@
+﻿using static HomeWork13.First;
+
+namespace HomeWork13
+{
+    public class First
+    {
+        public delegate void FirstHandler(bool prob);
+
+        public static int Multiply(int x, int y) => x * y;
+    }
+
+    public class Second
+    {
+        private int _result;
+        public Predicate<int> Calc(int x, int y, Func<int, int, int> func)
+        {
+            _result = func(x, y);
+
+            Predicate<int> predicate = Result;
+
+            return predicate;
+        }
+
+        public bool Result(int x)
+        {
+            return _result % x == 0;
+        }
+    }
+    internal class Program
+    {
+        static void Show(bool result)
+        {
+            Console.WriteLine($"result: {result}");
+        }
+        static void Main(string[] args)
+        {
+            Second second = new();
+
+            var tig = second.Calc(10, 5, Multiply);
+
+            FirstHandler firstHandler = Show;
+
+            firstHandler(tig(3));
+        }
+    }
+}
