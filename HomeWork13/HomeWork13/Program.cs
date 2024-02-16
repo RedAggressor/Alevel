dcc99ab90ebe41a1e054d919945b74948787e5b7
MaksymBabych <cadacred@gmail.com>
2024-02-16 11:33:27 +0200

    separate class

--- HomeWork13/HomeWork13/Program.cs
+++ HomeWork13/HomeWork13/Program.cs
@@ -2,37 +2,13 @@
 
 namespace HomeWork13
 {
-    public class First
-    {
-        public delegate void FirstHandler(bool prob);
-
-        public static int Multiply(int x, int y) => x * y;
-    }
-
-    public class Second
-    {
-        private int _result;
-
-        public Predicate<int> Calc(int x, int y, Func<int, int, int> func)
-        {
-            _result = func(x, y);
-
-            Predicate<int> predicate = Result;
-
-            return predicate;
-        }
-
-        public bool Result(int x)
-        {
-            return _result % x == 0;
-        }
-    }
     internal class Program
     {
-        static void Show(bool result)
+        private static void Show(bool result)
         {
             Console.WriteLine($"result: {result}");
         }
+
         static void Main(string[] args)
         {
             Second second = new Second();
@@ -45,9 +21,9 @@ namespace HomeWork13
 
             FirstHandler firstHandler = Show;
 
-            int divisionBy = 3;
+            int divisionByCompare = 3;
 
-            firstHandler(tig(divisionBy));
+            firstHandler(tig(divisionByCompare));
         }
     }
 }
