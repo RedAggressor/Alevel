cea01747d4ad1ad3fd0fccbcd6345d45da5ac003
MaksymBabych <cadacred@gmail.com>
2024-02-20 22:22:40 +0200

    add HomeWork 15

---
+++ HomeWork15/HomeWork15/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeWork15
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var task = Task.Run(() => AsyncReader.ConcatinationHelloWordAsync());
+
+            Console.WriteLine(task.Result);
+        }
+    }
+}
