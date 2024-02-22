df8a94bd8a9a103da900525bf6b94ca27f7caecd
MaksymBabych <cadacred@gmail.com>
2024-02-22 11:46:44 +0200

    add HomeWork 14

---
+++ HomeWork14/HomeWork14LINQ/HomeWork14LINQ/Contact.cs
@@ -0,0 +1,49 @@
+﻿namespace HomeWork14LINQ
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Number { get; set; }
+
+        public static string GetName()
+        {
+            var listName = new List<string>
+            {
+                "Anton", "Valera", "Dima", "Dinis",
+                "Vitalic", "Oleg", "Maxim", "Vlad",
+                "Yaroslav", "Alex"
+            };
+
+            int rendomndex = new Random().Next(0, listName.Count);
+
+            string name = listName.ElementAt(rendomndex);
+
+            return name;
+        }
+
+        public static string GetNumber()
+        {
+            string shablon = $"+38({new Random().Next(100, 1000)})-{new Random().Next(100, 1000)}-{new Random().Next(10, 100)}-{new Random().Next(10, 100)}";
+            return shablon;
+        }
+
+        public static int GetAge()
+        {
+            return new Random().Next(1, 100);
+        }
+
+        public static void ShowContacts(List<Contact> contacts)
+        {
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"Name: {contact.Name}, Age: {contact.Age}, Number: {contact.Number}");
+            }
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name ?? "none"}, Age: {Age}, Number: {Number ?? "none"}";
+                
+        }
+    }
+}
