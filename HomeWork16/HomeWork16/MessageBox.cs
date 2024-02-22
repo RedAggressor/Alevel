c2beefd69b5c9fe7221a8e54977c4305108fb3ee
MaksymBabych <cadacred@gmail.com>
2024-02-22 11:42:54 +0200

    add HomeWork 16

---
+++ HomeWork16/HomeWork16/MessageBox.cs
@@ -0,0 +1,22 @@
+﻿using HomeWork16.Enums;
+
+namespace HomeWork16;
+
+public delegate void MessageBoxDelegate (State state);
+
+internal class MessageBox
+{
+    public event MessageBoxDelegate? closedWindods;
+    public async Task Open()
+    {
+        Console.WriteLine("Window is open");
+
+        await Task.Delay(3000);
+
+        Console.WriteLine("Window was closed by the user");
+
+        var randomState = (State)new Random().Next(0, 2);
+
+        closedWindods?.Invoke(randomState);
+    }
+}
---
+++ HomeWork16/HomeWork16/Program.cs
@@ -0,0 +1,27 @@
+﻿using HomeWork16.Enums;
+
+namespace HomeWork16;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var messageBox = new MessageBox();
+
+        messageBox.closedWindods += (State state) =>
+        {
+            if (state == State.OK)
+            {
+                Console.WriteLine($"{state} - the operation has been confirmed");
+            }
+            else
+            {
+                Console.WriteLine($"{state} - the operation was rejected");
+            }
+        };
+
+        var task = Task.Run(() => messageBox.Open());
+
+        task.Wait();
+    }
+}
